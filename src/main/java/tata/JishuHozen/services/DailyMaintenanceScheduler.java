8d759c56b87a5d64f1c2ac9e56d7276bc415dc38
Prince Sharma <princesharma0816@gmail.com>
2026-06-24 10:18:14 +0530

    scheduler testing 3

--- src/main/java/tata/JishuHozen/services/DailyMaintenanceScheduler.java
+++ src/main/java/tata/JishuHozen/services/DailyMaintenanceScheduler.java
@@ -26,7 +26,7 @@ public class DailyMaintenanceScheduler
 
     private final maintenanceLogsRepo logsRepo;
     @Scheduled(
-            cron = "0 05 9 * * *",
+            cron = "0 20 10 * * *",
             zone = "Asia/Kolkata")
     public void populateDailyTasks()
     {
@@ -66,7 +66,7 @@ public class DailyMaintenanceScheduler
         }
     }
     @Scheduled(
-            cron = "0 10 9 * * *",
+            cron = "0 25 10 * * *",
             zone = "Asia/Kolkata")
     public void markMissedMachines()
     {
