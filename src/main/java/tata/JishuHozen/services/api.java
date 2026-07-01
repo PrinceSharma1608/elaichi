4ba151bfed5d2b443fe1b0a201ba9199329c4db0
Prince Sharma <princesharma0816@gmail.com>
2026-07-01 14:53:29 +0530

    teset

--- src/main/java/tata/JishuHozen/services/api.java
+++ src/main/java/tata/JishuHozen/services/api.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import tata.JishuHozen.Auth.AuthController;
 import tata.JishuHozen.Auth.AuthService;
@@ -110,7 +110,7 @@ public class api
         else
             return HttpStatus.BAD_REQUEST;
     }
-    @PostMapping("/audit")
+   /* @PostMapping("/audit")
     public ResponseEntity<String> createAudit(
             @RequestBody AuditRequestDTO dto,
             Authentication authentication)
@@ -122,5 +122,5 @@ public class api
                 userService.createAudit(
                         userId,
                         dto));
-    }
+    }*/
 }
