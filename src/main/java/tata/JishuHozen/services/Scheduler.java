39eae139569d9149f5dcb890b1c053fc0a0fd9d5
Prince Sharma <princesharma0816@gmail.com>
2026-07-12 13:04:55 +0530

    Revert "fix: clear historical missed entries from daily status at midnight"
    This reverts commit 6835a4135781f5b3ec7c6a1af1739cfb5d9ae5cc.

--- src/main/java/tata/JishuHozen/services/Scheduler.java
+++ src/main/java/tata/JishuHozen/services/Scheduler.java
@@ -36,7 +36,7 @@ public class Scheduler
         Every day at 12:00 AM
      */
     @Transactional
-    @Scheduled(cron = "0 0 0 * * *",
+    @Scheduled(cron = "0 01 00 * * *",
             zone = "Asia/Kolkata")
     public void midnightScheduler()
     {
@@ -55,11 +55,6 @@ public class Scheduler
                 CurrentDailyMaintenanceStatus
                         .MaintenanceStatus
                         .DONE_MANUALLY);
-
-        statusRepo.deleteByMaintenanceStatus(
-                CurrentDailyMaintenanceStatus
-                        .MaintenanceStatus
-                        .MISSED);
 
         /*
             Fetch all due checklists.
